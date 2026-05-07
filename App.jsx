f9408224cc5115f34aeda369a13340f7d87d0e6d
davanibuddha9-lgtm <davanibuddha9@gmail.com>
2026-05-07 17:39:06 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,37 @@
-# Hello
+import { useState } from "react";
+import "./style.css";
+
+export default function App() {
+  const [signup, setSignup] = useState(false);
+
+  return (
+    <div className="main">
+      <div className="card">
+        <h1>{signup ? "Create Account" : "Welcome Back"}</h1>
+
+        <form>
+          {signup && <input type="text" placeholder="Full Name" required />}
+
+          <input type="email" placeholder="Email" required />
+
+          <input
+            type="password"
+            placeholder="Password"
+            minLength="6"
+            required
+          />
+
+          <button>{signup ? "Sign Up" : "Login"}</button>
+        </form
+
+    
+  
+          <p onClick={() => setSignup(!signup)}>
+          {signup
+            ? "Already have an account? Login"
+            : "New here? Create account"}
+        </p>
+      </div>
+    </div>
+  );
+}
